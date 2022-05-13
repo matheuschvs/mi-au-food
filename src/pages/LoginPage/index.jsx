a2fe7013f8178d716f59ccf569019466eb4144b7
Thiago Oliveira <thiago.oliveira_@outlook.com>
2022-05-13 09:42:45 -0300

    feat: component/Button

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -3,7 +3,8 @@
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
-import { Input, Container, Form, Box, Button } from './style';
+import { Input, Container, Form, Box } from './style';
+import { Button } from '../../components/Button';
 
 export const LoginPage = () => {
   const forSchema = yup.object().shape({
