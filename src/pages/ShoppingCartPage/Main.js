d9fd97c383a07ff31ecd64e9ab1a82c2f08e3400
adrianonobrega26 <71836298+adrianonobrega26@users.noreply.github.com>
2022-05-13 17:03:54 -0300

    feat - parte de estilização bem encaminhada

---
+++ src/pages/ShoppingCartPage/Main.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  h2 {
+    margin-top: 50px;
+    margin-left: 100px;
+    font-size: 32px;
+  }
+  li {
+    display: flex;
+    border: 1px solid red;
+    border-radius: 20px;
+    margin-left: 100px;
+    margin-top: 50px;
+    background-color: white;
+  }
+  img {
+    width: 257px;
+    height: 228px;
+  }
+  h3 {
+    font-size: 32px;
+    width: 700px;
+    margin-top: 20px;
+  }
+  h4 {
+    position: relative;
+    top: 70px;
+    right: 125px;
+    width: 80px;
+    font-size: 32px;
+  }
+  h5 {
+    margin-left: 30px;
+    margin-right: 60px;
+    font-size: 32px;
+    font-weight: bold;
+  }
+`;
