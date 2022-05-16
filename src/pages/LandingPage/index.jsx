a553544df6539cee3c53eb21009fca1d7a1380fa
Matheus C <matheuschaves21@gmail.com>
2022-05-16 15:37:44 -0300

    feat: start landing page

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,36 +1,14 @@
-import { useAuth } from '../../context/auth';
-
 import { defaultAnimation, defaultTransition } from '../../utils/defaultMotion';
 
-import { Container } from './style';
+import { Container, Opening } from './style';
 
 export const LandingPage = () => {
-  const { signIn, signUp } = useAuth();
-
   return (
     <Container animate={defaultAnimation} transition={defaultTransition}>
-      <h1>Landing Page</h1>
-      <button
-        type="button"
-        onClick={() =>
-          signUp({
-            name: 'lojaTeste',
-            email: 'lojaTeste@gmail.com',
-            password: '123456',
-            type: 'shop',
-          })
-        }
-      >
-        User Sign Up
-      </button>
-      <button
-        type="button"
-        onClick={() =>
-          signIn({ email: 'lojaTeste@gmail.com', password: '123456' })
-        }
-      >
-        Sign In
-      </button>
+      <Opening>
+        <h1>Mi-Au Food</h1>
+        <h2>Seu pet feliz de verdade</h2>
+      </Opening>
     </Container>
   );
 };
--- src/pages/LandingPage/style.js
+++ src/pages/LandingPage/style.js
@@ -1,4 +1,33 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
-export const Container = styled(motion.main)``;
+export const Container = styled(motion.main)`
+  padding: 5.93rem 1.143rem;
+`;
+
+export const Opening = styled.section`
+  text-align: center;
+
+  background: var(--primary-color);
+
+  width: 100vw;
+  height: 13.14rem;
+  padding-top: 2.14rem;
+  margin-left: -1.143rem;
+
+  border-bottom-left-radius: 10.7rem;
+  border-bottom-right-radius: 10.7rem;
+
+  h1 {
+    font-size: 2.86rem;
+    color: var(--background);
+  }
+
+  h2 {
+    font-size: 1.43rem;
+    max-width: 14.3rem;
+    color: var(--background);
+
+    margin: 0 auto;
+  }
+`;
