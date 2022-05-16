b2801983af0f098dbc95cc3ba55e4493ceae6221
Matheus C <matheuschaves21@gmail.com>
2022-05-16 18:26:02 -0300

    feat: create cards section on landing page

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,6 +1,11 @@
 import { defaultAnimation, defaultTransition } from '../../utils/defaultMotion';
 
-import { Container, Opening } from './style';
+import { GoToButton } from '../../components/GoToButton';
+
+import catImg from '../../assets/cat.svg';
+import dogImg from '../../assets/dog.svg';
+
+import { Container, Opening, Cards } from './style';
 
 export const LandingPage = () => {
   return (
@@ -9,6 +14,22 @@ export const LandingPage = () => {
         <h1>Mi-Au Food</h1>
         <h2>Seu pet feliz de verdade</h2>
       </Opening>
+      <Cards>
+        <article>
+          <h3>Seu gato mais feliz com as rações premium</h3>
+          <div>
+            <img src={catImg} alt="um gato se espreguiçando" />
+            <GoToButton>Ver opções</GoToButton>
+          </div>
+        </article>
+        <article>
+          <h3>Os melhores brinquedos para pets do mercado</h3>
+          <div>
+            <GoToButton>Eu quero</GoToButton>
+            <img src={dogImg} alt="um cão atento" />
+          </div>
+        </article>
+      </Cards>
     </Container>
   );
 };
--- src/pages/LandingPage/style.js
+++ src/pages/LandingPage/style.js
@@ -31,3 +31,65 @@ export const Opening = styled.section`
     margin: 0 auto;
   }
 `;
+
+export const Cards = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  margin-top: 4.14rem;
+  margin-bottom: 11.43rem;
+
+  article {
+    width: 18.43rem;
+    height: 18.714rem;
+
+    padding: 2.5rem 1.71rem;
+
+    border-radius: 5px;
+
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    h3 {
+      font-size: 1.57rem;
+      color: var(--background);
+      text-align: center;
+      margin-bottom: 1.571rem;
+    }
+
+    &:first-child {
+      background: var(--secondary-color);
+
+      & > div {
+        display: flex;
+        align-items: flex-start;
+
+        button {
+          margin-top: 0.5rem;
+          margin-left: -2rem;
+        }
+      }
+    }
+
+    &:last-child {
+      margin-top: 6.857rem;
+      background: var(--tertiary-color);
+      align-self: flex-end;
+
+      & > div {
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
+
+        button {
+          background: var(--tertiary-color-1);
+          margin-right: -2rem;
+
+          &:hover {
+            background: var(--tertiary-color-2);
+          }
+        }
+      }
+    }
+  }
+`;
