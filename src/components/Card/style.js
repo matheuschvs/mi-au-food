44fe1290fbd199e97d4030ef83037f344cff89e4
beadebona <beatrizdebona.bd@gmail.com>
2022-05-17 14:15:05 -0300

    feat: Create Quantity controller component

--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -76,13 +76,3 @@ export const CardButton = styled.button`
     font-weight: bold;
   
 `
-export const RangeButton = styled.button`
-  width: 1.5rem;
-  height: 1.5rem;
-  border: none;
-  border-radius: 2px;
-  font-size: 1rem;
-  font-weight: bold;
-  background-color: var( --secondary-color);
-  color: var(--background);
-`
