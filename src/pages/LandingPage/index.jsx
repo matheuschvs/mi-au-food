8a4ba2185cd8d8256e61dcfb030245909d14dbd4
Matheus C <matheuschaves21@gmail.com>
2022-05-17 12:28:02 -0300

    feat: finish landing page style

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,38 +1,128 @@
+import { Instagram, GitHub, Linkedin } from 'react-feather';
+
+import { Link } from 'react-router-dom';
 import { defaultAnimation, defaultTransition } from '../../utils/defaultMotion';
 
 import { GoToButton } from '../../components/GoToButton';
+import { Button } from '../../components/Button';
 
 import catImg from '../../assets/cat.svg';
 import dogImg from '../../assets/dog.svg';
 
-import { Container, Opening, Cards, Partners } from './style';
+import {
+  Container,
+  Opening,
+  Cards,
+  Partners,
+  PartnersCount,
+  Footer,
+  Devs,
+  Contact,
+} from './style';
 
 export const LandingPage = () => {
   return (
-    <Container animate={defaultAnimation} transition={defaultTransition}>
-      <Opening>
-        <h1>Mi-Au Food</h1>
-        <h2>Seu pet feliz de verdade</h2>
-      </Opening>
-      <Cards>
-        <article>
-          <h3>Seu gato mais feliz com as rações premium</h3>
-          <div>
-            <img src={catImg} alt="um gato se espreguiçando" />
-            <GoToButton>Ver opções</GoToButton>
-          </div>
-        </article>
-        <article>
-          <h3>Os melhores brinquedos para pets do mercado</h3>
-          <div>
-            <GoToButton>Eu quero</GoToButton>
-            <img src={dogImg} alt="um cão atento" />
-          </div>
-        </article>
-      </Cards>
-      <Partners>
-        <h3>Torne-se um parceiro e encontre os clientes mais próximos</h3>
-      </Partners>
-    </Container>
+    <>
+      <Container animate={defaultAnimation} transition={defaultTransition}>
+        <Opening>
+          <h1>Mi-Au Food</h1>
+          <h2>Seu pet feliz de verdade</h2>
+        </Opening>
+        <Cards>
+          <article>
+            <h3>Seu gato mais feliz com as rações premium</h3>
+            <div>
+              <img src={catImg} alt="um gato se espreguiçando" />
+              <GoToButton>Ver opções</GoToButton>
+            </div>
+          </article>
+          <article>
+            <h3>Os melhores brinquedos para pets do mercado</h3>
+            <div>
+              <GoToButton>Eu quero</GoToButton>
+              <img src={dogImg} alt="um cão atento" />
+            </div>
+          </article>
+        </Cards>
+        <Partners>
+          <h3>Torne-se um parceiro e encontre os clientes mais próximos</h3>
+          <Button>Quero me registrar</Button>
+        </Partners>
+        <PartnersCount>
+          <h4>Já somos 2560 parceiros registrados!</h4>
+        </PartnersCount>
+      </Container>
+      <Footer>
+        <div>
+          <Devs>
+            <h4>Desenvolvido por:</h4>
+            <p>Thiago Brandão</p>
+            <Link target="_blank" to="/">
+              <Instagram />
+            </Link>
+            <Link target="_blank" to="/">
+              <GitHub />
+            </Link>
+            <Link target="_blank" to="/">
+              <Linkedin />
+            </Link>
+            <p>Beatriz Costa</p>
+            <Link target="_blank" to="/">
+              <Instagram />
+            </Link>
+            <Link target="_blank" to="/">
+              <GitHub />
+            </Link>
+            <Link target="_blank" to="/">
+              <Linkedin />
+            </Link>
+            <p>Matheus Chaves</p>
+            <Link target="_blank" to="/">
+              <Instagram />
+            </Link>
+            <Link target="_blank" to="/">
+              <GitHub />
+            </Link>
+            <Link target="_blank" to="/">
+              <Linkedin />
+            </Link>
+            <p>Adriano Nóbrega</p>
+            <Link target="_blank" to="/">
+              <Instagram />
+            </Link>
+            <Link target="_blank" to="/">
+              <GitHub />
+            </Link>
+            <Link target="_blank" to="/">
+              <Linkedin />
+            </Link>
+            <p>Lucas Golle</p>
+            <Link target="_blank" to="/">
+              <Instagram />
+            </Link>
+            <Link target="_blank" to="/">
+              <GitHub />
+            </Link>
+            <Link target="_blank" to="/">
+              <Linkedin />
+            </Link>
+          </Devs>
+          <Contact>
+            <h4>Entre em contato:</h4>
+            <h5>E-mail</h5>
+            <address>
+              <a href="mailto:contact@miaufood.com">contact@miaufood.com</a>
+            </address>
+            <p>
+              © Copyright 2022 - Mi-Au Food - Todos os direitos reservados Mi-Au
+              Food
+            </p>
+          </Contact>
+        </div>
+        <div>
+          <h4>#KenzieAcademy</h4>
+        </div>
+      </Footer>
+    </>
   );
 };
--- src/pages/LandingPage/style.js
+++ src/pages/LandingPage/style.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import dogBackground from '../../assets/dog-face-mobile.png';
 
 export const Container = styled(motion.main)`
-  padding: 5.93rem 1.143rem;
+  padding: 5.93rem 1.143rem 0;
 `;
 
 export const Opening = styled.section`
@@ -106,9 +106,110 @@ export const Partners = styled.section`
   padding: 4.643rem 0.714rem;
   width: 100vw;
   margin-left: -1.143rem;
+  text-align: center;
 
   h3 {
     color: var(--title);
+  }
+
+  button {
+    display: inline-flex;
+    min-width: 13rem;
+    background: var(--secondary-color-2);
+    border-color: var(--secondary-color-2);
+    font: 400 1.143rem 'Ubuntu', sans-serif;
+
+    margin-top: 1.143rem;
+  }
+`;
+
+export const PartnersCount = styled.section`
+  padding: 7.143rem 0;
+
+  h4 {
     text-align: center;
+    font-size: 1.143rem;
+  }
+`;
+
+export const Footer = styled.footer`
+  border-top: 3px solid var(--secondary-color-2);
+
+  & > div:first-child {
+    background: var(--secondary-color);
+    padding: 1.57rem 1.143rem;
+  }
+
+  & > div:last-of-type {
+    background: var(--secondary-color-2);
+    padding: 1.143rem 0;
+
+    text-align: center;
+    color: var(--tertiary-color-1);
+  }
+`;
+
+export const Devs = styled.section`
+  h4 {
+    font-size: 1rem;
+    color: var(--title);
+    margin-bottom: 1rem;
+  }
+
+  p {
+    display: inline-block;
+    width: 12rem;
+    margin-left: 1rem;
+    margin-bottom: 0.5rem;
+
+    font: 700 0.857rem 'Ubuntu', sans-serif;
+    line-height: 1rem;
+    vertical-align: top;
+  }
+
+  p:last-of-type {
+    margin-bottom: 1rem;
+  }
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+    color: var(--title);
+  }
+
+  a + a {
+    margin-left: 0.3rem;
+  }
+`;
+
+export const Contact = styled.footer`
+  font-weight: 700;
+  font-family: 'Ubuntu', sans-serif;
+  text-align: center;
+
+  h4 {
+    text-align: left;
+    font-size: 1rem;
+    color: var(--title);
+    margin-bottom: 1rem;
+  }
+
+  h5 {
+    color: var(--subtitle);
+    font-size: 0.857rem;
+    margin-bottom: 0.5rem;
+  }
+
+  address {
+    margin-bottom: 1.3rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--primary-color-2);
+  }
+
+  p {
+    line-height: 1rem;
   }
 `;
