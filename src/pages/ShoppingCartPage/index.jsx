03c3c14cea1a67ad42928e98f829a89fb790a9f0
adrianonobrega26 <71836298+adrianonobrega26@users.noreply.github.com>
2022-05-17 12:42:22 -0300

    a

--- src/pages/ShoppingCartPage/index.jsx
+++ src/pages/ShoppingCartPage/index.jsx
@@ -16,41 +16,6 @@ import botaoMaior from '../../img/Button Primary.svg';
 
 import lixoIcone from '../../img/trash-2.svg';
 
-const cart = [
-  {
-    name: 'Ração Seca Nutrilus Prime Frango para Cães Adultos de Raças Médias',
-    description:
-      'Contém em sua composição extrato de yucca, que atua na redução de odor das fezes;',
-    price: 198.65,
-    type: 'Comida',
-    img: 'https://www.petlove.com.br/images/products/250928/large/2697118_Nutrilus-Prime-Ra%C3%A7as-M%C3%A9dias.jpg?1636403533',
-    petType: 'Gato',
-    userId: 4,
-    id: 3,
-  },
-  {
-    name: 'Bowl Future Pet para Cães e Gatos',
-    description: 'Feito de aço inox',
-    price: 99.38,
-    type: 'Acessório',
-    img: 'https://www.petlove.com.br/images/products/251757/product/Bowl_Future_Pet_para_C%C3%A3es_e_Gatos_-_800_mL_2765001_%281%29.jpg?1642454399',
-    petType: 'Todos',
-    userId: 4,
-    id: 4,
-  },
-  {
-    name: 'Biscoito Suprema para Cães Adultos Raças Pequenas',
-    description:
-      'Sem corantes artificiais;Feito com carnes selecionadas;Macio e saboroso.',
-    price: 16.19,
-    type: 'Comida',
-    img: 'https://www.petlove.com.br/images/products/249351/product/Biscoito_Suprema_para_C%C3%A3es_Adultos_Ra%C3%A7as_Pequenas_2724993_%282%29.jpg?1634831655',
-    petType: 'Cachorro',
-    userId: 4,
-    id: 5,
-  },
-];
-
 export const ShoppingCartPage = () => {
   return (
     <div>
