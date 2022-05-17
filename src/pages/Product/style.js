950f29c1e74f309dc9f6c6631ac2e6a2d9cc35cc
beadebona <beatrizdebona.bd@gmail.com>
2022-05-17 01:32:49 -0300

    feat: Product-Page

---
+++ src/pages/Product/style.js
@@ -0,0 +1,77 @@
+/* eslint-disable*/
+
+import styled from 'styled-components';
+
+export const StyledMain = styled.main`
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+margin: 1rem;
+
+
+div{
+  height: 320px;
+  width: 95%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin: 0 30px;
+}
+
+section{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+figure{
+  width: 300px;
+  height: 300px;
+}
+img{
+  width: inherit;
+  border-radius: 1rem;
+}
+
+p{
+  font-family: "Ubuntu";
+  font-size: 1rem;
+}
+
+span{
+  font-family: "Ubuntu";
+  font-size: 1.5rem;
+  font-weight: bold;
+
+}
+
+@media( min-width: 768px ){
+  flex-direction: row;
+  div{
+    height: 360px;
+  }
+  figure{
+  width: 360px;
+  height: 360px;
+  }
+  section{
+    flex-direction: row;
+  }
+}
+`
+
+export const ProductButton = styled.button`
+font-family: "Ubuntu";
+padding: 0.5rem 1rem;
+border: none;
+border-radius: 1rem;
+background-color: var(--secondary-color);
+color: var(--background);
+margin: 20px;
+`
+export const Head3 = styled.h3`
+text-align: center;
+margin: 20px;
+`
