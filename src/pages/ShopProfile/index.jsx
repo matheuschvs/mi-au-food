d9474c0db43884e1c1245480c4aa8284176b8c42
beadebona <beatrizdebona.bd@gmail.com>
2022-05-19 16:12:55 -0300

    fix: Bugs in the shop profile page

--- src/pages/ShopProfile/index.jsx
+++ src/pages/ShopProfile/index.jsx
@@ -4,11 +4,13 @@ import { OrderCard } from '../../components/OrderCard';
 import { ProfileCard } from '../../components/ProfileCard';
 import { useAuth } from '../../context/auth';
 import { API } from '../../services/api';
-import { Main, Li } from './style';
+import { ProductButton } from '../Product/style';
+import { Main, Li, Order } from './style';
 
 export const ShopProfile = () => {
   const {user} = useAuth();
-  const [ordersAvailable, setOrdersAvailable] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [displayOrder, setDisplayOrder] = useState({});
   
   const token = localStorage.getItem('@mi-au-food:token');
 
@@ -19,39 +21,55 @@ export const ShopProfile = () => {
         Authorization: `Bearer ${token}`,
       },
     }).then(response => {
-      setOrdersAvailable(response.data);
+      setOrders(response.data);
     });
-  }, [token]);
+  }, []);
   
   return (
     <Main>
       <ProfileCard />
+      <h2>Serviços em aberto</h2>
+      <li>
+        <p><span>Informações do Pedido</span></p>
+      </li>
       <ul>
-        <h2>Serviços em aberto</h2>
-        <Li>
-          <p><span>Nome do Cliente</span></p>
-          <p><span>Status do Pedido</span></p>
-          <p><span>Valor do Pedido</span></p>
-        </Li>
-        {ordersAvailable.map((order)=>{
+        {orders.map((order)=>{
         if(order.status === 'Aguardando'){
-          return <OrderCard key={order.id} order={order}/>
+          return <OrderCard setOrders={setOrders} setDisplayOrder={setDisplayOrder} key={order.id} order={order}/>
         }
         })}
       </ul>
+      <h2>Serviços em andamento</h2>
+      <li>
+        <p><span>Informações do Pedido</span></p>
+      </li>
       <ul>
-        <h2>Serviços em andamento</h2>
-        <Li>
-          <p><span>Nome do Cliente</span></p>
-          <p><span>Status do Pedido</span></p>
-          <p><span>Valor do Pedido</span></p>
-        </Li>
-      {ordersAvailable.map((order)=>{
-        if(order.shopId == user.id){
-          return <OrderCard key={order.id} order={order}/>
+      {orders.map((order)=>{
+        if(order.shopId == user.id && order.status === 'Andamento'){
+          return <OrderCard setOrders={setOrders} setDisplayOrder={setDisplayOrder} key={order.id} order={order}/>
         }
         })}
       </ul>
+{!!displayOrder.id?(      
+  <Order>
+        <h3>Detalhes do Pedido</h3>
+        <p><span>Id do Pedido:</span> {displayOrder.id} </p>
+        <p> <span>Status do Pedido:</span> {displayOrder.status} </p>
+        <h3>Comprador</h3>
+          <p><span>Nome:</span>{displayOrder.user.name}</p>
+          <p><span>Endereço:</span>{displayOrder.user.address}</p>
+          <p><span>Telefone:</span>{displayOrder.user.tel}</p>
+          <h3>Produtos</h3>
+          <ul>
+          {displayOrder.product.map(ord=> <li key={ord.id}>
+          <p>{ord.name} </p>
+          <p>{ord.quantity}   R$ {ord.price} </p>
+        </li>)}
+          </ul>
+
+        <p><span>Valor Total:</span> R${displayOrder.totalCarrinho}</p>
+        <ProductButton onClick={() =>setDisplayOrder({})}>Fechar</ProductButton>
+      </Order>):(<></>)}
     </Main>
   );
 };
